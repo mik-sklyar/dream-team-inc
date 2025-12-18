521a75debceff1087a0253b0e3dafce75aa91220
Mikhail Sklyar <work@miksklyar.ru>
2025-12-18 18:27:17 +0700

    [task 8] Add abstract Strategy class with callback

---
+++ src/main/java/EmployeeOperationStrategy.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.function.Consumer;
+
+public abstract class EmployeeOperationStrategy implements ActionStrategy {
+    protected final List<Employee> inputData;
+    private final Consumer<List<Employee>> callback;
+
+    public EmployeeOperationStrategy(List<Employee> input, Consumer<List<Employee>> callback) {
+        this.inputData = input;
+        this.callback = callback;
+    }
+
+    public EmployeeOperationStrategy(Consumer<List<Employee>> callback) {
+        this(null, callback);
+    }
+
+    public EmployeeOperationStrategy() {
+        this(null);
+    }
+
+    @Override
+    public void execute() {
+        List<Employee> result = performOperation();
+        if (callback != null) {
+            callback.accept(result);
+        }
+    }
+
+    protected abstract List<Employee> performOperation();
+
+}
