3ef4964c8af952749d716bc0e2f28127ee5c1489
Toehisa <tigrera9999eagle@gmail.com>
2025-12-22 23:23:28 +0300

    [task 4]Refactor loader

--- src/main/java/RandomDataResourceLoader.java
+++ src/main/java/RandomDataResourceLoader.java
@@ -2,11 +2,40 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
-public class RandomDataResourceLoader {
-    protected static List<String> loadFileToList(Path directory, String fileName) {
+final class RandomDataResourceLoader {
+    private final static Path directory = Path.of("src", "main", "resources");
+
+    private final static List<String> maleNames;
+    private final static List<String> femaleNames;
+    private final static List<String> domains;
+    private final static List<String> works;
+
+    static {
+        maleNames = RandomDataResourceLoader.loadFileToList("male-names-list.txt");
+        femaleNames = RandomDataResourceLoader.loadFileToList("female-names-list.txt");
+        domains = RandomDataResourceLoader.loadFileToList("domain-list.txt");
+        works = RandomDataResourceLoader.loadFileToList("work-list.txt");
+    }
+
+    static List<String> getMaleNames() {
+        return maleNames;
+    }
+
+    static List<String> getFemaleNames() {
+        return femaleNames;
+    }
+
+    static List<String> getDomains() {
+        return domains;
+    }
+
+    static List<String> getWorks() {
+        return works;
+    }
+
+    private static List<String> loadFileToList(String fileName) {
         List<String> list = null;
 
         try {
