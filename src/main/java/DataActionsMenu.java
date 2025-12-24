57e770d606334ddab381ffe3ddfe5b9be28fadc8
Mikhail Sklyar <work@miksklyar.ru>
2025-12-24 21:17:38 +0700

    [task 7] additional sorting options

--- src/main/java/DataActionsMenu.java
+++ src/main/java/DataActionsMenu.java
@@ -28,11 +28,12 @@ public class DataActionsMenu {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("\n--- Меню обработки данных ---");
-            System.out.println("К работе готовы " + employees.size() + "сотрудников.");
+            System.out.println("К работе готовы " + employees.size() + " сотрудников.");
             System.out.println("1 - Вывести список на экран");
-            System.out.println("2 - Сортировать по имени");
-            System.out.println("3 - Сортировать по email");
-            System.out.println("4 - Сортировать по паролю");
+            System.out.println("2 - Сортировать по порядку");
+            System.out.println("3 - Сортировать по id");
+            System.out.println("4 - Сортировать по имени");
+            System.out.println("5 - Сортировать по email");
             System.out.println("9 - Эти не годятся, начать заново набирать команду");
             System.out.println("0 - Отказаться от всего этого и уйти");
             System.out.print("Выберите действие: ");
@@ -44,13 +45,16 @@ public class DataActionsMenu {
                     context.setStrategy(new EmployeePrintStrategy(employees));
                     break;
                 case "2":
-                    context.setStrategy(new EmployeeQuickSortStrategy(employees, EmployeeSortingField.NAME, this::handleSortResult));
+                    context.setStrategy(new EmployeeQuickSortStrategy(employees, EmployeeSortingField.ORDER, this::handleSortResult));
                     break;
                 case "3":
-                    context.setStrategy(new EmployeeQuickSortStrategy(employees, EmployeeSortingField.EMAIL, this::handleSortResult));
+                    context.setStrategy(new EmployeeQuickSortStrategy(employees, EmployeeSortingField.ID, this::handleSortResult));
                     break;
                 case "4":
-                    context.setStrategy(new EmployeeQuickSortStrategy(employees, EmployeeSortingField.PASSWORD, this::handleSortResult));
+                    context.setStrategy(new EmployeeQuickSortStrategy(employees, EmployeeSortingField.NAME, this::handleSortResult));
+                    break;
+                case "5":
+                    context.setStrategy(new EmployeeQuickSortStrategy(employees, EmployeeSortingField.EMAIL, this::handleSortResult));
                     break;
                 case "0":
                     context.setStrategy(new ExitStrategy());
